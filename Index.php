98a12d006eb26d94d48a50c9f7935cb3c9e1d085
abdul-roofiu <roefidraz@gmail.com>
2021-08-04 19:51:27 +0700

    Menambahkan Nama Abdul Roofi'u

--- Index.php
+++ Index.php
@@ -2,5 +2,6 @@
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
 echo "<h3>Ridwan Tommy Wijayanto (195411019)</h3>"
+echo "<h3>Abdul Roofi'u (195411042)</h3>"
 
 ?>
